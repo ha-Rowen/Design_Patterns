3739430057a78b2f915156c0fca47c7029c85ae3
DESKTOP-RNU182K\김종학 <jonghak.re@gmail.com>
2025-09-20 14:12:49 +0900

    update ignore

--- src/Creational_Patterns/Factory_Method/Item.java
+++
@@ -1,5 +0,0 @@
-package Creational_Patterns.Factory_Method;
-
-public interface Item {
-    void use();
-}
--- src/Structural_Patterns/Adapter/Adapter_Interface.java
+++
@@ -1,7 +0,0 @@
-package Structural_Patterns.Adapter;
-
-public interface Adapter_Interface {
-
-   void Adaptee_input(int number);
-   void Adaptee_print();
-}
